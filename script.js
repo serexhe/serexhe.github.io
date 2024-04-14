317a18bf096d0834ecada14707db6fed03eb2134
serexhe <107854003+serexhe@users.noreply.github.com>
2024-04-14 22:19:48 +0200

    script.js - - imgcont1

--- script.js
+++ script.js
@@ -9,16 +9,3 @@ image.addEventListener('mouseleave', function() {
     this.classList.remove('hover');
   }, 1000); // Match the transition duration
 });
-
-
-const image = document.querySelector('.image-container-1');
-
-image.addEventListener('mouseenter', function() {
-  this.classList.add('hover');
-});
-
-image.addEventListener('mouseleave', function() {
-  setTimeout(() => {
-    this.classList.remove('hover');
-  }, 1000); // Match the transition duration
-});
