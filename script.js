ef18542e39d26bc50492d0e2008827319216a125
serexhe <107854003+serexhe@users.noreply.github.com>
2024-04-14 23:20:46 +0200

    script.js - +query selector

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
-const image = document.querySelector('.image-container-2');
+const images = document.querySelectorAll('.image-container-1, .image-container-2');
 
-image.addEventListener('mouseenter', function() {
-  this.classList.add('hover');
-});
+images.forEach(image => {
+ image.addEventListener('mouseenter', function() {
+    this.classList.add('hover');
+ });
 
-image.addEventListener('mouseleave', function() {
-  setTimeout(() => {
-    this.classList.remove('hover');
-  }, 1000); // Match the transition duration
-});
-
-
-const image = document.querySelector('.image-container-1');
-
-image.addEventListener('mouseenter', function() {
-  this.classList.add('hover');
-});
-
-image.addEventListener('mouseleave', function() {
-  setTimeout(() => {
-    this.classList.remove('hover');
-  }, 1000); // Match the transition duration
+ image.addEventListener('mouseleave', function() {
+    setTimeout(() => {
+      this.classList.remove('hover');
+    }, 1000); // Match the transition duration
+ });
 });
