e5258315da5960467cdb20a5da4b70ab0ed17ab0
serexhe <107854003+serexhe@users.noreply.github.com>
2025-07-27 05:39:57 +0200

    secretofmana back

---
+++ secret-of-mana/schlifeartshirts/script.js
@@ -0,0 +1,13 @@
+const images = document.querySelectorAll('.image-container-1, .image-container-2, .fixed-element-2');
+
+images.forEach(image => {
+ image.addEventListener('mouseenter', function() {
+    this.classList.add('hover');
+ });
+
+ image.addEventListener('mouseleave', function() {
+    setTimeout(() => {
+      this.classList.remove('hover');
+    }, 1000); // Match the transition duration
+ });
+});
